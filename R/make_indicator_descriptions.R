32d6e68a1e99a06e7a0d1c35854bf12d31a8f4d1
Knight <aknight@usgs.gov>
2021-10-01 16:50:10 -0600

    final draft ordination and clustering demo

--- R/make_indicator_descriptions.R
+++ R/make_indicator_descriptions.R
@@ -49,6 +49,7 @@ make_indicator_descriptions <- function(indicators = c("AH_C3NativePerenGrassCov
     "AH_IntroducedPerenForbCover", "Biotic", "Introduced perennial forbs", "Percent foliar cover of the functional group",
     "AH_NativeAnnForbCover", "Biotic", "Native annual forbs", "Percent foliar cover of the functional group",
     "AH_IntroducedAnnForbCover", "Biotic", "Introduced annual forbs", "Percent foliar cover of the functional group",
+    "AH_ArtemisiaTridentataCover", "Biotic", "Artemisia tridentata", "Percent foliar cover of the functional group",
     "BareSoilCover", "Ecosystem structure", "Bare soil", "Percent cover of bare soil"
     )
 
