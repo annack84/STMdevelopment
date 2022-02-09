fd860d9528f00b6a34af52b13194a48b35d3ec81
Knight <aknight@usgs.gov>
2022-02-09 16:37:11 -0700

    allow greater map zoom for developers in EDIT_map

--- R/EDIT_map.R
+++ R/EDIT_map.R
@@ -1,9 +1,12 @@
 #' Create leaflet map of the target ESG
 #'
-#' @param target_ESG Character. Ecological Site Group to be mapped.
-#'   Follow the names used in the ESG raster released with Travis's 2021 manuscript.
-#' @param user Character. User name to generate input data file paths.
-#'   Options are "Anna", "Travis", and "VPN".
+#' @param target_ESG Character. Ecological Site Group to be mapped. Follow the
+#'   names used in the ESG raster released with Travis's 2021 manuscript.
+#' @param user Character. User name to generate input data file paths. Options
+#'   are "Anna", "Travis", and "VPN".
+#' @param maxZoomStyle Character. Use "public" for maps prepared for EDIT
+#'   (zooming limited to match ESG resampled map resolution). Use "developer" to
+#'   allow zooming in farther.
 #'
 #' @return
 #' @export
@@ -11,14 +14,19 @@
 #'
 
 EDIT_map <- function(target_ESG = "Semiarid_Warm_SandyUplands_LoamyUplands",
-                     user = "Anna"){
+                     user = "Anna",
+                     maxZoomStyle = "public"){
   # read in binary raster for target ESG
   target_ESG_raster <- raster::raster(file.path(data_file_paths(user)$target_ESG_map_folder,
                                              paste0(target_ESG, ".tif")))
 
+  # choose max zoom
+  if(maxZoomStyle=="public"){maxZ <- 11}
+  if(maxZoomStyle=="developer"){maxZ <- 13}
+
   # Create leaflet map
   map_figure <- leaflet::leaflet(options = leaflet::leafletOptions(zoomControl = TRUE,
-                                   minZoom = 3, maxZoom = 11)) %>%
+                                   minZoom = 3, maxZoom = maxZ)) %>%
     leaflet::addTiles() %>% # this chunk pulls in base maps
     leaflet::addProviderTiles("Esri.WorldTopoMap", group = "Topo") %>%
     leaflet::addProviderTiles("Esri.WorldImagery", group = "ESRI Aerial") %>%
